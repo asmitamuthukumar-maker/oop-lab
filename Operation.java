dd0011c71fd55689009f8de5439b316261148f57
asmita06 <asmitamuthukumar@gmail.com>
2026-01-04 17:42:25 +0530

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,14 @@
+class Operation{
+  public static void main(String[]args){
+       int a=13;
+       int b=7;
+       int sum=a+b;
+       System.out.println("sum="+sum);
+       int sub=a-b;
+       System.out.println("sub="+sub);
+       int multiply=a*b;
+       System.out.println("multiplication="+multiply);
+       float div=(float)a/b;
+       System.out.println("division="+div);
+  }
+} 
