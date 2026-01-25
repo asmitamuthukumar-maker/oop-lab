710ecaba29592c0fb114b9aec4f738849504d1f6
asmita06 <asmitamuthukumar@gmail.com>
2026-01-25 20:51:20 +0530

    Add files via upload

---
+++ Mark.java
@@ -0,0 +1,18 @@
+public class Mark{
+public static void main(String[]args){
+int x=3;
+switch (x){
+case 1:
+System.out.println("outstanding");
+break;
+case 2:
+System.out.println("topper");
+break;
+case 3:
+System.out.println("average student");
+break;
+default:
+System.out.println("need to improve");
+}
+}
+}
